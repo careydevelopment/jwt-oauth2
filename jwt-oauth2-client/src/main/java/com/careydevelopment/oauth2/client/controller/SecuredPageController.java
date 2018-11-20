09f98768dc7088d6d49320080bdde2dfb542ea0e
Brian Carey <mrbrianmcarey@gmail.com>
2018-11-20 16:00:35 -0500

    Added more tests

--- jwt-oauth2-client/src/main/java/com/careydevelopment/oauth2/client/controller/SecuredPageController.java
+++ jwt-oauth2-client/src/main/java/com/careydevelopment/oauth2/client/controller/SecuredPageController.java
@@ -1,5 +1,6 @@
 package com.careydevelopment.oauth2.client.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,9 @@ public class SecuredPageController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/securedPage")
     public String index(Model model) {
+    	Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    	System.err.println("principal here is " + principal);
+    	model.addAttribute("name", principal.toString());
         return "securedPage";
     }
 }
